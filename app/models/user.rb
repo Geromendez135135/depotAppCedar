00d069092812a6e373109706a1e99d318ff1534b
Geromendez135135 <geromendez135@gmail.com>
2023-09-19 11:27:37 -0300

    comentarios y cambios chicos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  after_destroy :ensure_an_admin_remains #supongo que se llama despues del destroy de la base pero antes de que termine la transaccion
+  after_destroy :ensure_an_admin_remains #supongo que se llama despues del destroy de la base pero antes de que termine la transaccion para que de esta forma se tenga como rollbackear el delete
 
   class Error < StandardError
   end
